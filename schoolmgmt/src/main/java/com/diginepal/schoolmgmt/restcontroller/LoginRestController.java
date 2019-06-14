7b0f048d0473a0b7195c0bb687ac67956c03a124
Shayuz <shayuzacharya7@gmail.com>
2019-06-14 19:43:43 +0545

    okayyy

---
+++ schoolmgmt/src/main/java/com/diginepal/schoolmgmt/restcontroller/LoginRestController.java
@@ -0,0 +1,15 @@
+package com.diginepal.schoolmgmt.restcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class LoginRestController {
+
+	@GetMapping
+	public ModelAndView index() {
+		return new ModelAndView("login/index");
+	}
+
+}
