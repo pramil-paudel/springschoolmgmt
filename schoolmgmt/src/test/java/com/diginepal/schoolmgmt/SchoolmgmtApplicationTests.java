0410f4065a2a281e785769831fce6c18ea3e2521
Administrator <Administrator@DESKTOP-I6IB67S>
2019-06-14 19:58:14 +0545

    ok

--- schoolmgmt/src/test/java/com/diginepal/schoolmgmt/SchoolmgmtApplicationTests.java
+++ schoolmgmt/src/test/java/com/diginepal/schoolmgmt/SchoolmgmtApplicationTests.java
@@ -8,9 +8,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SchoolmgmtApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
+	
 }
